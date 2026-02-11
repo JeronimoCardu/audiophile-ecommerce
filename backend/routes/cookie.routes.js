5576b46c476ac037ce662c58e1a88a5f89b725a9
Jeronimo Cardu <jeronimocardu29@gmail.com>
2026-02-11 15:36:05 -0300

    feat: cors permissions and new cookie route

---
+++ backend/routes/cookie.routes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// GET /cookies
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,17 @@ const PORT = process.env.PORT || 5000;
 
 // CORS middleware
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+// Cookies
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 // Body parser middleware
 app.use(express.json());
@@ -17,8 +27,11 @@ const connectDB = require("./config/db");
 connectDB();
 
 // Routes
-const router = require("./routes/product.routes.js");
-app.use("/api/products", router);
+const productsRoutes = require("./routes/product.routes.js");
+const cookiesRoutes = require("./routes/cookie.routes.js");
+
+app.use("/api/products", productsRoutes);
+app.use("/cookies", cookiesRoutes);
 
 // Listen on port
 app.listen(PORT, () => {
